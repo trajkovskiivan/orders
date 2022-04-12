32bc071f77c15523cb59f3327a1d46c22279badf
trajkovskiivan <58515706+trajkovskiivan@users.noreply.github.com>
2022-04-12 09:18:49 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const dbConnection = require("./db/dbSettings");
+const controller = require("./routes");
+
+const PORT = 5000;
+const port = normalizePort(process.env.PORT || PORT);
+
+const app = express();
+const server = http.createServer(app);
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api/orders", controller.orders);
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
